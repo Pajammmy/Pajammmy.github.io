c1694fb097ad2b64ff6b2de4d7a0639660d90d8d
Pajammmy <sknowlton@eastsideprep.org>
2023-10-27 00:26:39 -0700

    done

---
+++ forms demo/game.js
@@ -0,0 +1,53 @@
+//this is for my truths and lie game
+
+//global variable
+let fname = ""
+
+//my greet function
+function greet() {
+    let greetParagraph = document.getElementById("greet");
+
+    // get values from the form
+    fname = document.getElementById("fname").value;
+    let lastname = document.getElementById("lastname").value;
+    let age = document.getElementById("age").value;
+
+    greetParagraph.innerHTML = "Greetings " + age +
+        " year old " + fname + " " + lastname + "!";
+}
+
+function trivia() {
+    let triviaAnswer = document.getElementById("trivia-answer");
+    // get results of selected trivia answer
+    let chocolateSelected = document.getElementById("chocolate").checked;
+    let tunaSelected = document.getElementById("tuna").checked;
+    let honeySelected = document.getElementById("honey").checked;
+
+    if (chocolateSelected) {
+        triviaAnswer.innerHTML = fname + ", you are wrong";
+
+    } else if (tunaSelected) {
+        triviaAnswer.innerHTML = fname + ", you are wrong.";
+
+    } else {
+        triviaAnswer.innerHTML = fname + ", correct";
+    }
+}
+
+function game() {
+    let gameAnswer = document.getElementById("game-answer");
+    // get results of selected trivia answer
+    let obamaSelected = document.getElementById("obama").checked;
+    let jayzSelected = document.getElementById("jayz").checked;
+    let countrySelected = document.getElementById("country").checked;
+
+    if (obamaSelected) {
+        gameAnswer.innerHTML = fname + ", you are wrong";
+
+    } else if (jayzSelected) {
+        gameAnswer.innerHTML = fname + ", you are right.";
+
+    } else {
+        gameAnswer.innerHTML = fname + ", you are wrong";
+    }
+}
